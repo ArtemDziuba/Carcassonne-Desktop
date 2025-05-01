65cb84b4c747cd7f8ef44cc3d0b18c804de2ce9c
Artem Dziuba <artem.dziuba@hneu.net>
2025-05-01 16:09:08 +0300

    Ініціалізація проєкту Unity

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+        Screen.SetResolution(1980, 1080, false); // false = віконний режим
+    }
+
+    void Update()
+    {
+        // Логіка гри кожен кадр (тимчасово порожня)
+    }
+}
